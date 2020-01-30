626f5598bc2a0cff4413a9abf1adf36a8f55e3eb
Vladyslav Gulyaev <vlad040494@gmail.com>
2020-01-31 00:32:53 +0200

    still need to think about add feature...

--- src/views/MapPolygons.vue
+++ src/views/MapPolygons.vue
@@ -3,21 +3,32 @@
     <v-card flat class="pt-4" id="map" min-height="70vh">
     </v-card>
     <v-row class="justify-space-around align-end">
+
       <v-card flat class="pa-4" max-width="220px">
-        <v-radio-group v-model="addPolygonType" :disabled="!!deleteActivated || !!editActivated">
+        <v-btn :disabled="!!deleteActivated || !!editActivated" @click="createDrawingClass" width="100%" class="mb-4" color="info" >
+          Activate add mode
+        </v-btn>
+        <v-radio-group v-if="addActivated" v-model="addPolygonType">
           <v-radio label="Service available" value="ServiceAvailable" color="#A00E0D" />
           <v-radio label="Build commenced" value="BuildCommenced"/>
         </v-radio-group>
         <v-btn :disabled="!(addingPolygonId && addPolygonType)" color="success" @click="createPolygon" width="100%">Add polygon</v-btn>
       </v-card>
+
       <v-card flat class="pa-4" max-width="220px">
-        <v-checkbox v-model="editActivated" label="Activate edit mode" value="active" :disabled="!!deleteActivated || !!addPolygonType" />
+        <v-checkbox v-model="editActivated" label="Activate edit mode" value="active" :disabled="!!deleteActivated || addActivated" />
         <v-btn :disabled="!(editActivated)" color="warning" width="100%" @click="editPolygon">Edit polygon</v-btn>
       </v-card>
+
       <v-card flat class="pa-4" max-width="220px">
-        <v-checkbox v-model="deleteActivated" label="Activate delete mode" value="active" :disabled="!!editActivated || !!addPolygonType" />
+        <v-checkbox v-model="deleteActivated" label="Activate delete mode" value="active" :disabled="!!editActivated || addActivated" />
         <v-btn :disabled="!(deleteActivated)" color="error" width="100%" @click="deletePolygon">Delete polygon</v-btn>
       </v-card>
+
+      <v-card flat class="pa-4" max-width="220px">
+        <v-btn width="100%" @click="showServerData">Show server data</v-btn>
+      </v-card>
+
     </v-row>
   </v-container>
 </template>
@@ -34,9 +45,15 @@ export default {
     return {
       place: { lat: -37.85701362, lng: 144.963058 },
 
-      addPolygonType: '',
+      map: null,
+
+      drawing: null,
+
+      addActivated: false,
       editActivated: '',
       deleteActivated: '',
+
+      addPolygonType: '',
 
       addingPolygonId: 0,
 
@@ -70,12 +87,20 @@ export default {
   },
 
   methods: {
+    showServerData() {
+      console.log('ServiceAvailable', this.serverPolygonsAvailable)
+      console.log('BuildCommenced', this.serverPolygonsCommenced)
+      console.log(this.editedPolygonBufferAvail)
+    },
+
     initMap() {
       const map = new window.google.maps.Map(document.querySelector('#map'), {
         center: this.place,
         zoom: 13,
         styles: mapConfigs,
       })
+
+      this.map = map
 
       const state = new window.google.maps.Data()
       state.loadGeoJson(this.endpoint)
@@ -99,12 +124,34 @@ export default {
             if (typeOf === 'ServiceAvailable' && JSON.stringify(coordArr) !== JSON.stringify(this.serverPolygonsAvailable[id])) {
               this.editedPolygonAvail[id] = coordArr
               this.editedPolygonBufferAvail = polygon
-              console.log('ServiceAvailable has been changed')
             } else if (typeOf === 'BuildCommenced' && JSON.stringify(coordArr) !== JSON.stringify(this.serverPolygonsCommenced[id])) {
               this.editedPolygonCommenced[id] = coordArr
               this.editedPolygonBufferCommenced = polygon
-              console.log('BuildCommenced has been changed')
             }
+          }
+        })
+      }
+
+      const addListenersOnPolygonRightClick = (polygon, typeOf) => {
+        window.google.maps.event.addListener(polygon, 'rightclick', () => {
+          this.deletedPolygonAvail = ''
+          this.deletedPolygonCommenced = ''
+          this.deletedPolygonBufferAvail = null
+          this.deletedPolygonBufferCommenced = null
+          if (typeOf === 'ServiceAvailable') {
+            polygon.setOptions({
+              fillColor: '#A00E0D',
+              strokeColor: '#A00E0D',
+              fillOpacity: 0.3,
+              editable: false,
+            })
+          } else if (typeOf === 'BuildCommenced') {
+            polygon.setOptions({
+              fillColor: '#000000',
+              strokeColor: '#000000',
+              fillOpacity: 0.3,
+              editable: false,
+            })
           }
         })
       }
@@ -112,24 +159,25 @@ export default {
       const addListenersOnPolygonForDelete = (polygon, id, typeOf) => {
         window.google.maps.event.addListener(polygon, 'click', () => {
           if (this.deleteActivated) {
+            polygon.setOptions({
+              fillColor: '#D32F2F',
+              strokeColor: '#D32F2F',
+              fillOpacity: 0.7,
+            })
             if (typeOf === 'ServiceAvailable' && id === Object.keys(this.serverPolygonsAvailable).find(key => key === id)) {
               this.deletedPolygonAvail = id
               this.deletedPolygonBufferAvail = polygon
-              console.log('ServiceAvailable has been removed')
             } else if (typeOf === 'BuildCommenced' && id === Object.keys(this.serverPolygonsCommenced).find(key => key === id)) {
               this.deletedPolygonCommenced = id
               this.deletedPolygonBufferCommenced = polygon
-              console.log('BuildCommenced has been removed')
             }
           }
         })
       }
 
-      let avail = null
-      let build = null
       state.addListener('addfeature', (e) => {
         if (e.feature.getProperty('typeOf') === 'ServiceAvailable') {
-          avail = e.feature.getGeometry()
+          const avail = e.feature.getGeometry()
           const typeOf = e.feature.getProperty('typeOf')
           const paths = avail.getAt(0).getArray()
           const id = e.feature.getProperty('id')
@@ -145,8 +193,9 @@ export default {
           })
           addListenersOnPolygonForEdit(p, id, typeOf)
           addListenersOnPolygonForDelete(p, id, typeOf)
+          addListenersOnPolygonRightClick(p, typeOf)
         } else {
-          build = e.feature.getGeometry()
+          const build = e.feature.getGeometry()
           const typeOf = e.feature.getProperty('typeOf')
           const paths = build.getAt(0).getArray()
           const id = e.feature.getProperty('id')
@@ -161,9 +210,13 @@ export default {
           })
           addListenersOnPolygonForEdit(p, id, typeOf)
           addListenersOnPolygonForDelete(p, id, typeOf)
+          addListenersOnPolygonRightClick(p, typeOf)
         }
       })
+    },
 
+    createDrawingClass() {
+      this.addActivated = true
       const drawingManager = new window.google.maps.drawing.DrawingManager({
         drawingControlOptions: {
           position: window.google.maps.ControlPosition.BOTTOM_CENTER,
@@ -176,8 +229,10 @@ export default {
           editable: true,
         },
       })
-      drawingManager.setMap(map)
+      this.drawing = drawingManager
+      this.drawing.setMap(this.map)
       window.google.maps.event.addListener(drawingManager, 'polygoncomplete', (e) => {
+        console.log(e)
         const coordinates = e.getPath().getArray()
         const coordArr = []
         coordinates.forEach((elem) => {
@@ -192,15 +247,16 @@ export default {
     },
 
     createPolygon() {
-      if (this.addPolygonType === 'ServiceAvailable') {
-        this.serverPolygonsAvailable[this.addingPolygonId] = [this.addPolygon]
-        this.addPolygonType = ''
-        this.addingPolygonId = 0
-      } else if (this.addPolygonType === 'BuildCommenced') {
-        this.serverPolygonsCommenced[this.addingPolygonId] = [this.addPolygon]
-        this.addPolygonType = ''
-        this.addingPolygonId = 0
-      }
+
+      // if (this.addPolygonType === 'ServiceAvailable') {
+      //   this.serverPolygonsAvailable[this.addingPolygonId] = [this.addPolygon]
+      //   this.addPolygonType = ''
+      //   this.addingPolygonId = 0
+      // } else if (this.addPolygonType === 'BuildCommenced') {
+      //   this.serverPolygonsCommenced[this.addingPolygonId] = [this.addPolygon]
+      //   this.addPolygonType = ''
+      //   this.addingPolygonId = 0
+      // }
     },
 
     editPolygon() {
@@ -211,6 +267,10 @@ export default {
           editable: false,
         })
         this.editedPolygonBufferAvail = null
+        const editedId = Object.keys(this.serverPolygonsAvailable).find(id => this.editedPolygonAvail[id])
+        if (editedId) {
+          this.serverPolygonsAvailable[editedId] = this.editedPolygonAvail[editedId]
+        }
       } else if (this.editedPolygonBufferCommenced) {
         this.editedPolygonBufferCommenced.setOptions({
           fillColor: '#000000',
@@ -218,9 +278,13 @@ export default {
           editable: false,
         })
         this.editedPolygonBufferCommenced = null
+        const editedId = Object.keys(this.serverPolygonsCommenced).find(id => this.editedPolygonCommenced[id])
+        if (editedId) {
+          this.serverPolygonsCommenced[editedId] = this.editedPolygonCommenced[editedId]
+        }
       } else {
         // eslint-disable-next-line no-alert
-        alert("Click on the edited polygon before hitting 'Edit polygon' button")
+        alert("Click on the edited polygon before hitting 'Edit polygon' button, or click right mouse button to cancel editing this polygon")
       }
     },
 
@@ -239,7 +303,7 @@ export default {
         delete this.serverPolygonsCommenced[this.deletedPolygonCommenced]
       } else {
         // eslint-disable-next-line no-alert
-        alert("Click on the polygon you want to delete before hitting 'Delete polygon' button")
+        alert("Click on the polygon you want to delete before hitting 'Delete polygon' button, or click right mouse button to cancel deleting this polygon")
       }
     },
   },
