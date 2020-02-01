9a918df54e79408a5b730bf63cdfabafcde260f9
Vladyslav Gulyaev <vlad040494@gmail.com>
2020-02-01 02:58:57 +0200

    started markers, adding edit feature

--- src/views/MapAddresses.vue
+++ src/views/MapAddresses.vue
@@ -20,6 +20,12 @@
       </v-card>
 
       <v-card flat class="pa-4" max-width="220px">
+        <!-- <v-btn width="100%" class="mb-4" @click="showInfo">{{infoShown ? "Close info" : "Show info"}}</v-btn> -->
+        <!-- <v-btn width="100%" color="accent" :disabled="addActivated || !!editActivated || !!deleteActivated || checkApplyButton()" @click="applyChanges">Apply changes</v-btn> -->
+        <v-btn width="100%" color="accent" @click="applyChanges">Apply changes</v-btn>
+      </v-card>
+
+      <v-card flat class="pa-4" max-width="220px">
         <v-btn  width="100%" @click="showData">Show state data</v-btn>
       </v-card>
     </v-row>
@@ -42,6 +48,9 @@ export default {
       editActivated: '',
       deleteActivated: '',
 
+      editedMarker: null,
+      editedMarkerId: '',
+
       stateMarkers: {},
       initialServerMarkers: {},
     }
@@ -56,7 +65,10 @@ export default {
   methods: {
 
     showData() {
+      console.log('%c editActivated: ', 'color: #F00; font-weight: bold;', this.editActivated)
       console.log('%c stateMarkers: ', 'color: #FF6F00; font-weight: bold;', this.stateMarkers)
+      console.log('%c initialServerMarkers: ', 'color: #FF6F00; font-weight: bold;', this.initialServerMarkers)
+      console.log('%c states are equal: ', 'color: #FF6;', JSON.stringify(this.initialServerMarkers) === JSON.stringify(this.stateMarkers))
     },
 
     addListenersOnMarkerForEdit(marker, id) {
@@ -64,7 +76,14 @@ export default {
         if (this.editActivated) {
           marker.setOptions({
             draggable: true,
-            label: 'Edited',
+          })
+          // console.log(123)
+          window.google.maps.event.addListener(marker, 'drag', () => {
+            marker.setOptions({
+              label: 'Editing',
+            })
+            this.editMarker = marker
+            this.editMarkerId = id
           })
         }
       })
@@ -86,11 +105,11 @@ export default {
         const geo = e.feature.getGeometry()
         let coordinates
         const paths = geo.forEachLatLng((elem) => { coordinates = [+elem.lat().toFixed(5), +elem.lng().toFixed(5)] })
-        // console.log(e.feature)
+        this.stateMarkers[id] = { address, coordinates }
+        this.initialServerMarkers[id] = { address, coordinates: [...coordinates] }
         const marker = new window.google.maps.Marker({
           position: { lat: coordinates[0], lng: coordinates[1] },
           map,
-          // draggable: true,
         })
         this.addListenersOnMarkerForEdit(marker, id)
       })
@@ -101,11 +120,39 @@ export default {
     },
 
     editMarker() {
-      console.log('edited')
+      this.editMarker.setOptions({
+        label: 'Edited',
+        draggable: false,
+      })
+      const coordinates = [+this.editMarker.getPosition().toJSON().lat.toFixed(5), +this.editMarker.getPosition().toJSON().lng.toFixed(5)]
+      // this.editMarker.getPosition().forEachLatLng((elem) => { coordinates = [+elem.lat().toFixed(5), +elem.lng().toFixed(5)] })
+      console.log(coordinates)
+      this.editMarker = null
     },
 
     deleteMarker() {
       console.log('deleted')
+    },
+
+    async applyChanges() {
+      await this.$store.dispatch('map/SAVE_POINTS', {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {
+              id: 'pin1571042912106',
+              address: '1,Loch Avenue,St Kilda East,Glen Eira City,Victoria,Australia,3183',
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: [
+                145.00711,
+                -37.87582,
+              ],
+            },
+          }],
+      })
     },
   },
   mounted() {
