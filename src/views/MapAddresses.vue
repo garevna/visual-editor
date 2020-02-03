a5bd960660219e020e1b77b9753157bb3c6c8c80
Vladyslav Gulyaev <vlad040494@gmail.com>
2020-02-03 11:52:03 +0200

    need to add json

--- src/views/MapAddresses.vue
+++ src/views/MapAddresses.vue
@@ -25,9 +25,9 @@
         <v-btn width="100%" color="accent" @click="applyChanges">Apply changes</v-btn>
       </v-card>
 
-      <v-card flat class="pa-4" max-width="220px">
+      <!-- <v-card flat class="pa-4" max-width="220px">
         <v-btn  width="100%" @click="showData">Show state data</v-btn>
-      </v-card>
+      </v-card> -->
     </v-row>
 
   </v-container>
@@ -35,13 +35,12 @@
 
 <script>
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-console */
 
 import { mapGetters } from 'vuex'
 
 import mapConfigs from '@/configs/map'
 
-const arr = require('../store/modules/markers.json')
+const initialMarkers = require('../storage/markers.json')
 
 export default {
   data() {
@@ -74,12 +73,12 @@ export default {
 
   methods: {
 
-    showData() {
-      console.log('%c addingMarker: ', 'color: #1B5E20; font-weight: bold;', this.addingMarker)
-      console.log('%c stateMarkers: ', 'color: #FF6F00; font-weight: bold;', this.stateMarkers)
-      console.log('%c initialServerMarkers: ', 'color: #FF6F00; font-weight: bold;', this.initialServerMarkers)
-      console.log('%c states are equal: ', 'color: #FF6;', JSON.stringify(this.initialServerMarkers) === JSON.stringify(this.stateMarkers))
-    },
+    // showData() {
+    //   console.log('%c addingMarker: ', 'color: #1B5E20; font-weight: bold;', this.addingMarker)
+    //   console.log('%c stateMarkers: ', 'color: #FF6F00; font-weight: bold;', this.stateMarkers)
+    //   console.log('%c initialServerMarkers: ', 'color: #FF6F00; font-weight: bold;', this.initialServerMarkers)
+    //   console.log('%c states are equal: ', 'color: #FF6;', JSON.stringify(this.initialServerMarkers) === JSON.stringify(this.stateMarkers))
+    // },
 
     checkApplyButton() {
       return (JSON.stringify(this.stateMarkers) === JSON.stringify(this.initialServerMarkers))
@@ -262,6 +261,7 @@ export default {
       this.stateMarkers = {}
       this.initialServerMarkers = {}
 
+      // await this.$store.dispatch('map/SAVE_POINTS', initialMarkers)
       await this.$store.dispatch('map/SAVE_POINTS', result)
       this.initMap()
     },
